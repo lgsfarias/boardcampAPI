9ba696533757fef6322dac194d4f1bfdce7c955a
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-05-25 22:03:00 -0300

    fix: retunr error 409 when categorie already exist and 400 when name is empty

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -14,11 +14,21 @@ export default class Categories {
 
     static createCategory = async (req, res) => {
         const { name } = req.body;
+        if (!name) {
+            return res.status(400).send('Name is required');
+        }
         try {
-            const category = await conection.query(
-                'INSERT INTO categories (name) VALUES ($1)',
+            const alrearyExists = await conection.query(
+                'SELECT * FROM categories WHERE name = $1',
                 [name]
             );
+            if (alrearyExists.rows.length > 0) {
+                return res.status(409).send('Category already exists');
+            }
+
+            await conection.query('INSERT INTO categories (name) VALUES ($1)', [
+                name,
+            ]);
             return res.sendStatus(201);
         } catch (error) {
             return res.status(500).json({ message: error.message });
