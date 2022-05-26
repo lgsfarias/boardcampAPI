b5f979b3db7618a34c25af85f5ff841fe256a436
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-05-25 22:44:35 -0300

    feat: add game name search with query string

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -2,12 +2,15 @@ import conection from '../config/database.js';
 
 export default class Games {
     static getGames = async (req, res) => {
+        const { name } = req.query;
         try {
             const games = await conection.query(
                 `SELECT games.*,
                 categories.name AS "categoryName" 
                     FROM games 
-                    JOIN categories ON games."categoryId" = categories.id`
+                    JOIN categories ON games."categoryId" = categories.id
+                    WHERE games.name ~* $1`,
+                [name ? name : '']
             );
             return res.status(200).json(games.rows);
         } catch (error) {
