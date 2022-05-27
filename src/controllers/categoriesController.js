ed6d4aed2939bc8685bdfe4814eb88dcf737a952
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-05-26 23:03:27 -0300

    feat: add pagination to get categories route

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -2,10 +2,16 @@ import connection from '../config/database.js';
 
 export default class Categories {
     static getCategories = async (req, res) => {
+        const offset = req.query.offset || 0;
+        const limit = req.query.limit || 10000;
+        //FIXME: encontrar uma forma mais elegante pro limit
         try {
+            const query = 'SELECT * FROM categories';
             const categories = await connection.query(
-                'SELECT * FROM categories'
+                `${query} OFFSET $1 LIMIT $2`,
+                [offset, limit]
             );
+
             return res.status(200).json(categories.rows);
         } catch (error) {
             return res.status(500).json({ message: error.message });
